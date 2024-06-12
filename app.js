3360a52f6c0f19cf838ce2349b339c89132977fe
myKollege <mykollegeindia@gmail.com>
2024-06-12 14:52:03 +0600

    giftcard removed

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
@@ -9,14 +11,25 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const body_parser_1 = __importDefault(require("body-parser"));
 //============ api routes ====================
 const user_route_1 = __importDefault(require("./apis/module/user/user.route"));
-const category_route_1 = __importDefault(require("./apis/module/category/category.route"));
+const category_route_1 = __importDefault(
+  require("./apis/module/category/category.route")
+);
 const faq_route_1 = __importDefault(require("./apis/module/faq/faq.route"));
-const product_route_1 = __importDefault(require("./apis/module/products/product.route"));
-const productKit_route_1 = __importDefault(require("./apis/module/productsKits/productKit.route"));
-const campaign_route_1 = __importDefault(require("./apis/module/campaign/campaign.route"));
-const giftcard_route_1 = __importDefault(require("./apis/module/giftCard/giftcard.route"));
-const transaction_route_1 = __importDefault(require("./apis/module/transactions/transaction.route"));
-const GiftCardTemplate_route_1 = __importDefault(require("./apis/module/giftCardTemplate/GiftCardTemplate.route"));
+const product_route_1 = __importDefault(
+  require("./apis/module/products/product.route")
+);
+const productKit_route_1 = __importDefault(
+  require("./apis/module/productsKits/productKit.route")
+);
+const campaign_route_1 = __importDefault(
+  require("./apis/module/campaign/campaign.route")
+);
+const transaction_route_1 = __importDefault(
+  require("./apis/module/transactions/transaction.route")
+);
+const GiftCardTemplate_route_1 = __importDefault(
+  require("./apis/module/giftCardTemplate/GiftCardTemplate.route")
+);
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 //middle ware
@@ -34,7 +47,6 @@ app.use("/api/v1/faq", faq_route_1.default);
 app.use("/api/v1/product", product_route_1.default);
 app.use("/api/v1/product_kit", productKit_route_1.default);
 app.use("/api/v1/campaign", campaign_route_1.default);
-app.use("/api/v1/gift_card", giftcard_route_1.default);
 app.use("/api/v1/transaction", transaction_route_1.default);
 app.use("/api/v1/gift_card_template", GiftCardTemplate_route_1.default);
 exports.default = app;
